79fa80748a9ab8088baab54ce7ac713312746e45
ofersarid <ofersarid@gmail.com>
2020-09-24 14:05:47 +0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Page } from '../components';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
