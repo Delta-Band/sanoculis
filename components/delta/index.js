10a976de6e941eb657a55e719d41adc9bca1c84d
ofersarid <ofersarid@gmail.com>
2021-05-10 17:22:58 +0300

    wip

--- components/delta/index.js
+++ components/delta/index.js
@@ -1,4 +1,4 @@
 export { default as MadeByDelta } from './MadeByDelta';
 export { default as TextBlocks } from './TextBlocks';
-export { default as Modal } from './modal';
+export { default as Modal } from './Modal';
 export { default as SectionLayout } from './SectionLayout';
