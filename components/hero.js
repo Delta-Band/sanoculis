285683cf0186318948b80869dff97e8b9e187974
ofersarid <ofersarid@gmail.com>
2021-05-24 12:52:05 +0300

    video v10

--- components/hero.js
+++ components/hero.js
@@ -2,11 +2,41 @@ import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import cx from 'classnames';
-import { Delta2ColLayout } from '../shared';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  logo: { width: '100%', marginTop: -theme.spacing(5) },
+  heroRoot: {
+    position: 'relative',
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(14),
+    background: theme.palette.primary.main,
+    overflow: 'hidden',
+    [theme.breakpoints.up('md')]: {
+      paddingBottom: theme.spacing(0)
+    }
+  },
+  content: {
+    width: '100vw',
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    marginTop: theme.spacing(8),
+    [theme.breakpoints.up('sm')]: {
+      width: '80vw',
+      margin: '0 auto',
+      marginTop: theme.spacing(8)
+    },
+    [theme.breakpoints.up('md')]: {
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      marginTop: theme.spacing(3),
+      transform: 'translate(5%, -50%)',
+      width: '40vw',
+      maxWidth: 700
+    },
+    [theme.breakpoints.up('lg')]: {}
+  },
+  logo: { width: '100%' },
   tagline: {
     fontSize: 32,
     lineHeight: '38px',
@@ -25,25 +55,14 @@ const useStyles = makeStyles((theme) => ({
   },
   video: {
     width: '100%',
-    marginBottom: '40px',
-    marginTop: theme.spacing(8),
-    [theme.breakpoints.up('md')]: {
-      marginBottom: 0,
-      marginTop: theme.spacing(0)
-    }
+    objectFit: 'cover'
   },
-  gradientOverlay: {
-    background:
-      'linear-gradient(-90deg, rgba(97, 114, 255, 1) 0%, rgba(97, 114, 255, 0) 100%)',
-    width: 50,
-    height: '100%',
+  wave: {
     position: 'absolute',
-    top: 0,
-    right: 0,
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'block'
-    }
+    width: '100%',
+    bottom: 0,
+    left: 0,
+    transform: 'translateY(65%)'
   }
 }));
 
@@ -53,49 +72,29 @@ function Hero({ tagline, description }) {
   const upMD = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
-    <Delta2ColLayout
-      paddingTop={upMD ? -2 : -10}
-      paddingBottom={upMD ? -8 : 5}
-      background={theme.palette.primary.main}
-      fullWidth
-      art={
-        <div style={{ position: 'relative ' }}>
-          <video
-            playsInline
-            muted
-            className={classes.video}
-            src='videos/hero.mp4'
-            autoPlay
-          />
-          <div className={classes.gradientOverlay} />
-        </div>
-      }
-      extendBottomWith={
-        <img
-          src='images/white_wave.png'
-          style={{
-            width: '100%',
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            transform: 'translateY(50%)'
-          }}
-        />
-      }
-      content={[
-        <img key={0} src='mims_for_hero.svg' className={classes.logo} />,
-        <Typography key={1} className={cx(classes.white, classes.tagline)}>
+    <div className={classes.heroRoot}>
+      <video
+        playsInline
+        muted
+        loop
+        className={classes.video}
+        src={`videos/${upMD ? 'desktop' : 'mobile'}/hero.mp4`}
+        autoPlay
+      />
+      <div className={classes.content}>
+        <img src='mims_for_hero.svg' className={classes.logo} />
+        <Typography className={cx(classes.white, classes.tagline)}>
           {tagline}
-        </Typography>,
+        </Typography>
         <Typography
-          key={2}
           className={classes.white}
           style={{ whiteSpace: 'pre-line', marginTop: theme.spacing(4) }}
         >
           {description}
         </Typography>
-      ]}
-    />
+      </div>
+      <img src='images/white_wave.png' className={classes.wave} />
+    </div>
   );
 }
 
