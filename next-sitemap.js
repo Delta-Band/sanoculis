1ca8d9a31fc120b2e84e0458b3397531b5517a62
ofersarid <ofersarid@gmail.com>
2021-06-15 18:56:54 +0300

    added site map generator small fix 2

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://sanoculis.com',
+  siteUrl: 'https://sanoculis.com',
   generateRobotsTxt: true // (optional)
   // ...other options
 };
